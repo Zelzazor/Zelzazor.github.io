9a8192859503ecbbd318008755c4a8d67930ba88
Zelzazor <zelzazor.ramirez62@gmail.com>
2020-07-28 00:20:55 -0400

    Hecho refactoring a Random Person API: implementando async/await

--- RandomPersonAPI/index.js
+++ RandomPersonAPI/index.js
@@ -1,27 +1,24 @@
-const btnGenerate = document.getElementById('btnGenerate')
-const name = document.getElementById('name')
-const emailCont = document.getElementById('email')
-const countryCont = document.getElementById('country')
-const imgCont = document.getElementById('api-img')
+const btnGenerate = document.getElementById('btnGenerate');
+const name = document.getElementById('name');
+const emailCont = document.getElementById('email');
+const countryCont = document.getElementById('country');
+const imgCont = document.getElementById('api-img');
 
-const generateAPI = () => {
-    fetch('https://randomuser.me/api/')
-    .then((res)=>res.json())
-    .then((res)=>res.results[0])
-    .then((res)=>{
-        const {
-            name: {first},
-            name: {last},
-            location: {country},
-            email,
-            picture: {large}
-        } = res;
+const generateAPI = async () => {
+    const response = await fetch('https://randomuser.me/apo/');
+    const json = await response.json();
+    const result = await json.results[0];
+    const {name: {first}, name: {last}, location: {country}, email, picture: {large}} = result;
     name.innerHTML = `${first} ${last}`;
     emailCont.innerHTML = email;
     countryCont.innerHTML = country;
     imgCont.src = large;
-    })
 }
 
-generateAPI();
+generateAPI().catch((error)=>{
+    console.log("There was an error!");
+    console.error(error);
+});
+
+
 btnGenerate.addEventListener('click', generateAPI);
